c8880ac8def5f5738751f065b8a03780f914e829
Sohaib Aktar Gazi <aktar19sohaib@gmail.com>
2023-08-14 15:07:43 +0530

    initial commit

---
+++ prac.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+// Coded by Sohaib Aktar
+ 
+
+int main(){
+    
+    int a =10;
+    int b= 5;
+    cout<< a+b <<endl;
+
+return 0;
+}
